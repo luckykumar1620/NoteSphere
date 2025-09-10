6b1973db35b06a21cf41210a7b8053ed83f30309
Lucky Kumar <lucky9110132241@gmail.com>
2025-09-10 16:59:51 +0530

    fix: remove frontend serving code from backend

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 const express =require('express') 
 const connectToMongo=require('./db');
 const cors=require('cors')
-const path = require("path");
+
 const app = express();
 
 const port = process.env.PORT || 5000;
@@ -16,11 +16,6 @@ app.use(express.json());
 app.use('/api/auth',require('./routes/auth'))
 app.use('/api/notes',require('./routes/notes'))
 
-app.use(express.static(path.join(__dirname, "frontend/build")));
-
-app.get(/.*/, (req, res) => {
-  res.sendFile(path.join(__dirname, "frontend/build", "index.html"));
-});
 
 app.listen(port,()=>{
     console.log(`NoteSphere is listening on port ${port}`)
